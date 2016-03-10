c030f427c480827452e72b20af90e0ad2251339f
Soumya Ray <soumya.ray@gmail.com>
2016-03-11 07:17:42 +0800

    ruby_to_json: refactor to more traditional OOP style

--- ruby_to_json.rb
+++ ruby_to_json.rb
@@ -15,23 +15,37 @@ puts books.to_json
 
 require 'json'
 
-##
-# Represent books as native Ruby objects but provide descriptive to_json
-class Books
-  def []=(title, author)
-    @books ||= {}
-    @books[title] = author
+class Book
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
   end
 
-  def to_json
-    @books.map do |title, author|
-      { 'title' => title, 'author' => author }
-    end.to_json
+  def to_json(*options)
+    { 'title' => title, 'author' => author }.to_json(*options)
   end
 end
 
-mybooks = Books.new
-mybooks['The Unbearable Lightness of Being'] = 'Milan Kundera'
-mybooks['Seven Pillars of Wisdom'] = 'Milan Kundera'
+my_book = Books.new('The Unbearable Lightness of Being', 'Milan Kundera')
+puts my_book.to_json
 
-puts mybooks.to_json
+class Books
+  def initialize
+    @collection = []
+  end
+
+  def add_book(title, author)
+    @collection << Book.new(title, author)
+  end
+
+  def to_json(*options)
+    {library: @collection }.to_json(options)
+  end
+end
+
+my_library = Books.new
+my_library.add_book('The Unbearable Lightness of Being', 'Milan Kundera')
+my_library.add_book('Seven Pillars of Wisdom', 'T.E. Lawrence')
+puts my_library.to_json
